f932188847636e54477689aba65c7c7afe21730a
Shahbaz Ali <engr.shahbaz92@gmail.com>
2022-03-23 16:38:20 +0200

    snap shot test cases fixed

--- src/_tests/login.test.tsx
+++ src/_tests/login.test.tsx
@@ -1,25 +1,29 @@
 import React from "react";
-import { render, RenderResult, } from "@testing-library/react";
+import { render, RenderResult } from "@testing-library/react";
 import Enzyme from "enzyme";
 import LoginPage from "../views/login/login";
 
 import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
 const { createMemoryHistory } = require("history");
 import { Router } from "react-router-dom";
+import { Provider } from "react-redux";
+import configureStore from "redux-mock-store";
 
 Enzyme.configure({ adapter: new Adapter() });
 
-
 describe("Login page tests", () => {
+  const initialState = { output: 10 };
+  const mockStore = configureStore();
   let history = createMemoryHistory();
   let wrapper: RenderResult;
-  beforeEach(() => {
-    wrapper = render(
+  let store = mockStore(initialState);
+  wrapper = render(
+    <Provider store={store}>
       <Router location={history.location} navigator={history}>
         <LoginPage />
       </Router>
-    );
-  });
+    </Provider>
+  );
   it("snapshot login view", async () => {
     expect(wrapper).toMatchSnapshot();
   });
--- src/_tests/posts.test.tsx
+++ src/_tests/posts.test.tsx
@@ -1,19 +1,24 @@
-import React from 'react';
-import {
-    render,
-  } from "@testing-library/react";
-  import Enzyme from "enzyme";
-  
-  import Posts from "../views/posts/posts";
-  
-  import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
-  
-  Enzyme.configure({ adapter: new Adapter() });
-  
-  describe("<Posts/>", () => {
-    it("renders with Props", async () => {
-      const wrapper = render(<Posts/>);
-      expect(wrapper).toMatchSnapshot();
-    });
+import React from "react";
+import { render, RenderResult } from "@testing-library/react";
+import Enzyme from "enzyme";
+
+import Posts from "../views/posts/posts";
+
+import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
+const { createMemoryHistory } = require("history");
+import { Router } from "react-router-dom";
+
+Enzyme.configure({ adapter: new Adapter() });
+
+describe("<Posts/>", () => {
+  it("renders with Props", async () => {
+    let history = createMemoryHistory();
+    let wrapper: RenderResult;
+    wrapper = render(
+      <Router location={history.location} navigator={history}>
+        <Posts />
+      </Router>
+    );
+    expect(wrapper).toMatchSnapshot();
   });
-  
+});
