46ad599179d508cb0683d999daef8eb09dbcc9d2
Shahbaz Ali <engr.shahbaz92@gmail.com>
2022-03-27 20:12:43 +0300

    code commented

--- src/constant/constant.tsx
+++ src/constant/constant.tsx
@@ -1,2 +1,3 @@
-export const clientId = "ju16a6m81mhid5ue1z3v2g0uh";
+// there is no api through which I can get clientId so I assume that It will remain constant
+export const clientId = "ju16a6m81mhid5ue1z3v2g0uh"; 
 export const url = "https://api.supermetrics.com/assignment";
